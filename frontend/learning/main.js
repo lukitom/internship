0e9b369f6f7836dcbd3723d58f0448145a83f985
ThePlayMK <theplaymk@gmail.com>
2022-07-15 08:20:16 +0200

    WI1-48: - Moved comments to Readme.txt file.

--- frontend/learning/main.js
+++ frontend/learning/main.js
@@ -1,15 +1,6 @@
-//const siema = 'cześć';
-//console.log(siema.toUpperCase());
-//console.log(siema.substring(0, 2).toUpperCase());
-//let gry = ['Fortnite', 'Minecraft', 'CS:GO', 'Tomb Rider'];
-//console.log(gry);
-//console.log(gry[2*2-1]);
 function Add() {
     const x = document.getElementById("number1").value;
     const y = document.getElementById("number2").value;
-    //Number(x);
-    //Number(y);
-    //const z = x + y;
     console.log(+x + +y);
 }
 function Multiply() {
@@ -17,14 +8,3 @@ function Multiply() {
     let y = document.getElementById("number2").value;
     console.log(+x * +y)
 }
-
-//Link used:
-//https://dev.to/sanchithasr/7-ways-to-convert-a-string-to-number-in-javascript-4l
-//https://www.programiz.com/javascript/examples/add-number
-//https://www.mikedane.com/web-development/javascript/getting-user-input/
-//https://www.educative.io/answers/how-to-add-an-id-to-element-in-javascript
-//https://www.w3schools.com/js/js_input_examples.asp
-//https://www.w3schools.com/jsref/met_console_log.asp
-//https://www.w3schools.com/jsref/event_onclick.asp
-//https://www.w3schools.com/js/js_comments.asp
-//https://youtu.be/hdI2bqOjy3c?t=1620
