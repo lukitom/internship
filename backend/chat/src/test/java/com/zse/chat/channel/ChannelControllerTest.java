4688b273a30c9eb6df23dc2d2137204bb6d5aa2b
lukitom <tomaszek.l03@gmail.com>
2022-07-22 09:10:31 +0200

    WI1-56: - modify preparing test in ChannelControllerTest

--- backend/chat/src/test/java/com/zse/chat/channel/ChannelControllerTest.java
+++ backend/chat/src/test/java/com/zse/chat/channel/ChannelControllerTest.java
@@ -1,25 +1,34 @@
 package com.zse.chat.channel;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.zse.chat.message.MessageFixture;
 import com.zse.chat.user.User;
 import com.zse.chat.user.UserFixture;
 import com.zse.chat.user.UserService;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ChannelController.class)
 class ChannelControllerTest {
 
     @Autowired
     private ObjectMapper mapper;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Autowired
     private ChannelController channelController;
@@ -36,26 +45,31 @@ class ChannelControllerTest {
 
     //region GET("/channels")
     @Test
-    public void shouldReturnAllMessagesForOwnerInChannel() {
+    public void shouldReturnAllMessagesForOwnerInChannel() throws Exception {
         User user = UserFixture.createDefaultUser(1).build();
 
-        List<User> owners = UserFixture.createListOfDefaultUser(10);
-        List<User> members = UserFixture.createListOfDefaultUser(20, 20);
-
         List<Channel> channels = new ArrayList<>();
-        channels.add(
-                ChannelFixture.createDefaultChannel(
-                        1,
-                        owners,
-                        members,
-                        MessageFixture.createListOfMessages(5)
-                ).build()
-        );
 
-        when(userService.getUserByNick("testNick"))
-                .thenReturn(user);
-        when(channelService.getChannels(user))
+        for (int i = 0; i < 5; i++) {
+            channels.add(
+                    ChannelFixture.createDefaultChannel(
+                            i + 1,
+                            null,
+                            null,
+                            null
+                    ).build()
+            );
+        }
+
+        when(userService.getUserByNick("testNickname1")).thenReturn(user);
+        when(channelService.getChannels(ArgumentMatchers.any(User.class)))
                 .thenReturn(channels);
+
+        mockMvc.perform(get("/channels"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(5)));
+
     }
     //endregion
 
